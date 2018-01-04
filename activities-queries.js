316b6eaa28cf2dfe04cc1b4b1db9b20f36e7468b
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-01-04 15:59:47 +0100

    pg pool optimisation

--- activities-queries.js
+++ activities-queries.js
@@ -17,49 +17,42 @@ function getAllActivities(pool, callback) {
 
 function createActivity(form, pool) {
   return pool.query(
-    "INSERT INTO activities (id,title,description,status) VALUES ($1::uuid,$2::text,$3::text,$4::text) RETURNING id",
-    [uuidv4(),form.title,form.description,"Open"]
-  )
-  .then(res => {
-    let joinObject = {activityId: res.rows[0].id};
-    return joinObject;
-  })
-  .then(res => {
-    return pool.query(
-      "INSERT INTO users (id,name,firstname,login,password) VALUES ($1::uuid,$2::text,$3::text,$4::text,$5::text) RETURNING id",
-      [uuidv4(),"toto",form.attendee,"toto","titi"]
+      "INSERT INTO activities (id,title,description,status) VALUES ($1::uuid,$2::text,$3::text,$4::text) RETURNING id", [uuidv4(), form.title, form.description, "Open"]
     )
-    .then(res2 => {
-      res.userId = res2.rows[0].id;
-      return res
+    .then(res => {
+      let joinObject = {
+        activityId: res.rows[0].id
+      };
+      return joinObject;
     })
-    .then(finalRes =>
-      pool.query(
-          "insert into activities_users Values ($1::uuid,$2::uuid,$3::boolean)",
-          [finalRes.activityId, finalRes.userId, true]
+    .then(res => {
+      return pool.query(
+          "INSERT INTO users (id,name,firstname,login,password) VALUES ($1::uuid,$2::text,$3::text,$4::text,$5::text) RETURNING id", [uuidv4(), "toto", form.attendee, "toto", "titi"]
         )
-    )
-    .catch(e => console.log(e))
-  })
+        .then(res2 => {
+          res.userId = res2.rows[0].id;
+          return res
+        })
+        .then(finalRes =>
+          pool.query(
+            "insert into activities_users Values ($1::uuid,$2::uuid,$3::boolean)", [finalRes.activityId, finalRes.userId, true]
+          )
+        )
+        .catch(e => console.log(e))
+    })
 }
 
 
-function getActivityDetails(id) {
-  const client = new PG.Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: utils.isPgSslActive(),
-  });
-  client.connect()
-  return client.query("SELECT * from activities where id = $1 ",
-    [id]
-  );
+function getActivityDetails(id, pool) {
+
+  return pool.query("SELECT * from activities where id = $1 ", [id]);
 
 }
 
 
 
 module.exports = {
-  getAllActivities:getAllActivities,
-  createActivity:createActivity,
-  getActivityDetails:getActivityDetails
+  getAllActivities: getAllActivities,
+  createActivity: createActivity,
+  getActivityDetails: getActivityDetails
 };
