f2fcbe0fc1e9724cd5d87f37db6347e088e7e70c
FABIEN GLORY <fabien.glory@decathlon.com>
2018-01-04 14:31:38 +0100

    rebase ok

--- activities-queries.js
+++ activities-queries.js
@@ -1,5 +1,6 @@
 const PG = require("pg");
 const utils = require("./utils.js");
+const uuidv4 = require('uuid/v4');
 
 function getAllActivities(callback) {
   const client = new PG.Client({
@@ -20,4 +21,28 @@ function getAllActivities(callback) {
   );
 }
 
-module.exports = {getAllActivities:getAllActivities};
+
+function createActivity(form) {
+  const client = new PG.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: utils.isPgSslActive(),
+});
+  client.connect();
+  return client.query(
+    "INSERT INTO activities (id,title,description,status) VALUES ($1,$2,$3,$4)",
+    [uuidv4(),form.title,form.description,"Open"]);
+    // [form.title,form.description],
+    // function(error, resultQuery) {
+    //   if (error) {
+    //     return error;
+    //   } else {
+    //     return resultQuery;
+    //   }
+    //  client.end();
+    }
+
+
+module.exports = {
+  getAllActivities:getAllActivities,
+  createActivity:createActivity
+};
