2ebfd5d605d05c28bd68a52f72db200c6b61d952
Hamza Ali <hamza.ali@decathlon.com>
2018-01-06 09:12:13 +0100

    another fix for test file

--- .__tests__/utils.test.js
+++ .__tests__/utils.test.js
@@ -2,9 +2,9 @@ const utils = require("../utils.js")
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  user: 'sharepay2dfh_test',
+  user: 'sharepay2fdh_test',
   host: 'localhost',
-  database: 'sharepay2dfh_test',
+  database: 'sharepay2fdh_test',
   password: '',
   port: 5432,
 })
